b3127ef5eee451f0ac0e04040c1f9dcbfde12167
uaLogan <lglin@cs.ualberta.ca>
2014-01-17 13:41:10 -0700

    STABLE: added list view to counter list view

--- gen/ca/ualberta/Logan/counter/R.java
+++ gen/ca/ualberta/Logan/counter/R.java
@@ -18,12 +18,16 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int title_vertical_margin=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int ListLayout=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int listTitle=0x7f080002;
+        public static final int listView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_counter=0x7f030000;
