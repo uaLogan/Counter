af13d35de6457a339ebccc136959eb92cf8e548b
uaLogan <lglin@cs.ualberta.ca>
2014-01-27 13:49:46 -0700

    Started stats activity

---
+++ src/ca/ualberta/Logan/counter/StatsActivity.java
@@ -0,0 +1,16 @@
+package ca.ualberta.Logan.counter;
+
+
+public class StatsActivity extends BaseActivity
+{
+	private Storage storage;
+
+	
+	
+	@Override
+	protected void recoverData()
+	{
+		
+	}
+
+}
